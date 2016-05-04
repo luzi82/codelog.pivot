b33664122d7cce992dde8818018ee0d0241c774f
Luzi82 <luzi82@gmail.com>
2016-05-04 23:23:45 +0800

    add getTreeViewBranchListeners

--- src/main/java/com/luzi82/codelog/pivottest/TreeDemo.java
+++ src/main/java/com/luzi82/codelog/pivottest/TreeDemo.java
@@ -1,11 +1,13 @@
 package com.luzi82.codelog.pivottest;
 
 import org.apache.pivot.collections.Map;
+import org.apache.pivot.collections.Sequence.Tree.Path;
 import org.apache.pivot.wtk.Application;
 import org.apache.pivot.wtk.ApplicationContext;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
 import org.apache.pivot.wtk.TreeView;
+import org.apache.pivot.wtk.TreeViewBranchListener;
 import org.apache.pivot.wtk.Window;
 import org.apache.pivot.wtk.content.TreeBranch;
 import org.apache.pivot.wtk.content.TreeNode;
@@ -28,31 +30,32 @@ public class TreeDemo implements Application {
 		window.setTitle("codelog.pivot Tree");
 		window.setMaximized(true);
 
-		new Thread() {
+		ApplicationContext.scheduleRecurringCallback(new Runnable() {
+			@Override
 			public void run() {
-				try {
-					while (true) {
-						ApplicationContext.queueCallback(new Runnable() {
-							@Override
-							public void run() {
-								String t = "" + System.currentTimeMillis();
-								TreeNode node = new TreeNode(t);
-								treeRoot1.add(node);
-								if (treeRoot1.getLength() > 10) {
-									treeRoot1.remove(0, treeRoot1.getLength() - 10);
-								}
-								for (TreeNode tn : treeRoot1) {
-									tn.setText(tn.getText() + ".");
-								}
-							}
-						});
-						Thread.sleep(1000);
-					}
-				} catch (Throwable t) {
-					t.printStackTrace();
+				String t = "" + System.currentTimeMillis();
+				TreeNode node = new TreeNode(t);
+				treeRoot1.add(node);
+				if (treeRoot1.getLength() > 10) {
+					treeRoot1.remove(0, treeRoot1.getLength() - 10);
+				}
+				for (TreeNode tn : treeRoot1) {
+					tn.setText(tn.getText() + ".");
 				}
 			}
-		}.start();
+		}, 1000);
+
+		treeView.getTreeViewBranchListeners().add(new TreeViewBranchListener() {
+			@Override
+			public void branchExpanded(TreeView treeView, Path path) {
+				System.err.println("EFGCIPPB branchExpanded " + path.toString());
+			}
+
+			@Override
+			public void branchCollapsed(TreeView treeView, Path path) {
+				System.err.println("SUEWRQNZ branchCollapsed " + path.toString());
+			}
+		});
 
 		window.open(arg0);
 	}
